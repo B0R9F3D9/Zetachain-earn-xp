00e64f0f555d019294e3d5324c668dde3d64350a
Уэйд Уилсон <131712860+B0R9F3D9@users.noreply.github.com>
2024-02-21 20:44:50 +0200

    Update zetachain.py
    rework

--- modules/zetachain.py
+++ modules/zetachain.py
@@ -11,7 +11,8 @@ from eth_account.messages import encode_structured_data
 from .account import Account
 from settings import *
 from config import *
-from utils import retry
+from .helpers import retry
+from .okx import OKX
 
 
 class Zetachain:
@@ -22,31 +23,37 @@ class Zetachain:
         self.izumi_router_contract: AsyncContract = self.acc.get_contract(CONTRACTS['izumi_router'], IZUMI_ROUTER_ABI)
         self.zetahub_contract: AsyncContract = self.acc.get_contract(CONTRACTS['zetahub'], ZETAHUB_ABI)
         self.headers = {
-            "Accept": "application/json, text/plain, */*",
-            "Origin": "https://hub.zetachain.com",
-            'User-Agent': UserAgent(os='windows').random,
+            'accept': 'application/json, text/plain, */*',
+            'origin': 'https://hub.zetachain.com',
+            'referer': 'https://hub.zetachain.com/',
+            'user-agent': UserAgent().random
         }
 
     @retry
     async def check_complete(self, task_name: str) -> bool:
         async with aiohttp.ClientSession(headers=self.headers) as session:
-            async with session.get(f'https://xp.cl04.zetachain.com/v1/get-user-has-xp-to-refresh?address={self.acc.address}') as resp:
-                if resp.status != 200: logger.error(f'{self.acc.info} Код: {resp.status} | Ответ: {await resp.text()}'); return
+            async with session.get(f'https://xp.cl04.zetachain.com/v1/get-user-has-xp-to-refresh', params={'address': self.acc.address}, proxy=self.acc.proxy) as resp:
+                if resp.status == 400 and 'not verified' in (await resp.text()).lower():
+                    raise Exception(f'{self.acc.info} Кошелёк не зарегистрирован! | Повторная проверка через 20-30 сек')
+                elif resp.status == 429:
+                    raise Exception(f'{self.acc.info} Лимит запросов! | Повторная попытка через 20-30 сек')
+                elif resp.status != 200: logger.error(f'{self.acc.info} Код: {resp.status} | Ответ: {await resp.text()}'); return
                 data = (await resp.json())['xpRefreshTrackingByTask'][task_name]
         return data['hasAlreadyEarned'] is False and data['hasXpToRefresh'] is False
     
-    @retry
     async def check_enroll(self) -> bool:
         async with aiohttp.ClientSession(headers=self.headers) as session:
-            async with session.post(f"https://xp.cl04.zetachain.com/v1/enroll-in-zeta-xp", json={'address': self.acc.address}) as resp:
-                if resp.status != 200: logger.error(f'{self.acc.info} Код: {resp.status} | Ответ: {await resp.text()}'); return
-                return (await resp.json())['isUserVerified'] is True
-
+            async with session.post(f"https://xp.cl04.zetachain.com/v1/enroll-in-zeta-xp", json={'address': self.acc.address}, proxy=self.acc.proxy) as resp:
+                if resp.status == 429: raise Exception(f'{self.acc.info} Лимит запросов! | Повторная попытка через 20-30 сек')
+                elif resp.status != 200: raise Exception(f'{self.acc.info} Код: {resp.status} | Ответ: {await resp.text()}')
+                return (await resp.json())['isUserVerified']
+        
     @retry
     async def enroll(self) -> None:
-        if await self.check_enroll() and not DO_ACTION_ANYWAY:
-            logger.info(f'{self.acc.info} Кошелёк уже зарегистрирован ✅')
-            return False
+        if not DO_ACTION_ANYWAY:
+            if await self.check_enroll():
+                logger.info(f'{self.acc.info} Кошелёк уже зарегистрирован ✅')
+                return False
         logger.info(f'{self.acc.info} Делаю регистрацию...')
         address, expiration, r, s, v = b64decode(REF_LINK.split('code=')[1][:-1]).decode().split('&')
         address = address.split('=')[1][2:]
@@ -57,27 +64,32 @@ class Zetachain:
         data = f"0xb9daad50000000000000000000000000{address.lower()}00000000000000000000000000000000000000000000000000000000{expiration}00000000000000000000000000000000000000000000000000000000000000{v}{r}{s}"
         tx_data = await self.acc.get_tx_data() | {'to': CONTRACTS['enroll'], 'data': data}
         await self.acc.send_txn(tx_data)
-        return True
+        await asyncio.sleep(10)
+        result = await self.check_enroll()
+        if result: return True
+        else: raise Exception(f'{self.acc.info} Регистрация не удалась!')
 
     @retry
     async def transfer(self) -> None:
-        if not await self.check_complete('SEND_ZETA') and not DO_ACTION_ANYWAY:
-            logger.info(f'{self.acc.info} Кошелёк уже переводил ZETA ✅')
-            return
+        if not DO_ACTION_ANYWAY:
+            if not await self.check_complete('SEND_ZETA'):
+                logger.info(f'{self.acc.info} Кошелёк уже переводил ZETA ✅')
+                return
         amount = random.uniform(*TRANSFER_AMOUNT)
         await self.acc.transfer_zeta(amount)
 
     @retry
-    async def izumi_swap(self, way: str) -> None:
-        if not await self.check_complete(f'RECEIVE_{way.split("/")[1].split(".")[0]}') and not DO_ACTION_ANYWAY:
-            logger.info(f'{self.acc.info} Кошелёк уже свапал {way} ✅')
-            return
-        fee = {'ZETA/BNB.BSC': 10000, 'ZETA/ETH.ETH': 3000, 'ZETA/BTC.BTC': 10000}[way]
+    async def izumi_swap(self, pair: str) -> None:
+        if not DO_ACTION_ANYWAY:
+            if not await self.check_complete(f'RECEIVE_{pair.split("/")[1].split(".")[0]}'):
+                logger.info(f'{self.acc.info} Кошелёк уже свапал {pair} ✅')
+                return
+        fee = {'ZETA/BNB.BSC': 10000, 'ZETA/ETH.ETH': 3000, 'ZETA/BTC.BTC': 10000}[pair]
         amount = random.uniform(*SWAP_AMOUNT)
-        logger.info(f'{self.acc.info} Делаю свап ZETA {way}...')
+        logger.info(f'{self.acc.info} Делаю свап {amount:.5f} {pair}')
 
-        from_token_bytes = HexBytes(TOKENS[way.split('/')[0]]).rjust(20, b'\0')
-        to_token_bytes = HexBytes(TOKENS[way.split('/')[1]]).rjust(20, b'\0')
+        from_token_bytes = HexBytes(SWAP_TOKENS[pair.split('/')[0]]).rjust(20, b'\0')
+        to_token_bytes = HexBytes(SWAP_TOKENS[pair.split('/')[1]]).rjust(20, b'\0')
         fee_bytes = fee.to_bytes(3, 'big')
         path = from_token_bytes + fee_bytes + to_token_bytes
 
@@ -87,29 +99,31 @@ class Zetachain:
                 path,
                 self.acc.address,
                 int(amount*10**18),
-                10 if way == 'ZETA/ETH.ETH' or way == 'ZETA/BNB.BSC' else 3,
-                int((await self.w3.eth.get_block("latest"))['timestamp'] + 3600))
-            ]
+                10 if pair in ['ZETA/ETH.ETH', 'ZETA/BNB.BSC'] else 3,
+                int((await self.w3.eth.get_block("latest"))['timestamp'] + 3600)
+            )]
         )
         tx_data = self.izumi_router_contract.encodeABI(fn_name='multicall', args=[[data, "0x12210e8a"]])
         txn = await self.acc.get_tx_data(int(amount*10**18)) | {'to': CONTRACTS['izumi_router'], 'data': tx_data}
         await self.acc.send_txn(txn)
 
     @retry
-    async def add_liquidity(self) -> None:
-        if not await self.check_complete('POOL_DEPOSIT_ANY_POOL') and not DO_ACTION_ANYWAY:
-            logger.info(f'{self.acc.info} Кошелёк уже добавлял ликвидность ✅')
-            return False
+    async def add_liquidity(self, pair: str) -> None:
+        if not DO_ACTION_ANYWAY:
+            if not await self.check_complete('POOL_DEPOSIT_ANY_POOL'):
+                logger.info(f'{self.acc.info} Кошелёк уже добавлял ликвидность ✅')
+                return False
 
-        bnb_balance = (await self.acc.get_balance('BNB.BSC'))['balance_wei']
-        amount_wei = int(bnb_balance * random.uniform(0.1, 0.9))
+        token_to = pair.split('/')[1]
+        token_balance = (await self.acc.get_balance(token_to))['balance_wei']
+        amount_wei = int(token_balance * random.uniform(0.1, 0.9)) # кол-во добавляемое в ликву - 10-90% от баланса токена
         zeta_amount_wei = int(amount_wei * random.uniform(0.5, 3)) # вроде лучше высчитать по курсу, но пох
         approve_amount = int(random.uniform(0.1, 111) * 10**18) # или amount_wei, но удобнее аппрув на дохуя 
-        await self.acc.approve(approve_amount, TOKENS['BNB.BSC'], CONTRACTS['zetahub'])
+        await self.acc.approve(approve_amount, USUAL_TOKENS[token_to], CONTRACTS['zetahub'])
 
-        logger.info(f'{self.acc.info} Добавляю {amount_wei/10**18:.7f} BNB + {zeta_amount_wei/10**18:.7f} ZETA в ликвидность...')
+        logger.info(f'{self.acc.info} Добавляю {amount_wei/10**18:.7f} {token_to} + {zeta_amount_wei/10**18:.7f} ZETA в ликвидность...')
         txn = await self.zetahub_contract.functions.addLiquidityETH(
-            TOKENS['BNB.BSC'],
+            USUAL_TOKENS[token_to],
             amount_wei,
             0, 0,
             self.acc.address,
@@ -139,7 +153,7 @@ class Zetachain:
     @retry
     async def claim(self) -> None:
         async with aiohttp.ClientSession(headers=self.headers) as session:
-            async with session.get(f'https://xp.cl04.zetachain.com/v1/get-user-has-xp-to-refresh?address={self.acc.address}') as resp:
+            async with session.get(f'https://xp.cl04.zetachain.com/v1/get-user-has-xp-to-refresh?address={self.acc.address}', proxy=self.acc.proxy) as resp:
                 if resp.status != 200: logger.error(f'{self.acc.info} Ответ сервера: {resp.status}'); return
                 data = await resp.json()
             tasks = [[key, value['xp']] for key, value in data["xpRefreshTrackingByTask"].items() if value["hasXpToRefresh"]]
@@ -150,35 +164,40 @@ class Zetachain:
             logger.info(f'{self.acc.info} Клеймлю {sum([task[1] for task in tasks])} поинтов...')
             points = 0
             for task in tasks:
-                signature = await self.sign_message()
-                json_data = {'address': self.acc.address, 'signedMessage': signature, 'task': task[0]}
-                async with session.post('https://xp.cl04.zetachain.com/v1/xp/claim-task', json=json_data) as resp:
-                    data = await resp.json()
-                if data['message'] == 'XP refreshed successfully':
-                    points += task[1]
-                    logger.info(f'{self.acc.info} Заклеймил {task[1]} поинтов за {task[0]} ✅')
-                else:
-                    logger.error(f'{self.acc.info} Не удалось заклеймить поинты для {task[0]}! Ответ: {data}')
-                await asyncio.sleep(random.uniform(5, 7))
+                for attempt in range(RETRY_COUNT):
+                    signature = await self.sign_message()
+                    json_data = {'address': self.acc.address, 'signedMessage': signature, 'task': task[0]}
+                    async with session.post('https://xp.cl04.zetachain.com/v1/xp/claim-task', json=json_data, proxy=self.acc.proxy) as resp:
+                        data = await resp.json()
+                    if data['message'] == 'XP refreshed successfully':
+                        points += task[1]
+                        logger.info(f'{self.acc.info} Заклеймил {task[1]} поинтов за {task[0]} ✅')
+                        await asyncio.sleep(random.uniform(7, 10))
+                        break
+                    else:
+                        logger.error(f'{self.acc.info} Не удалось заклеймить поинты для {task[0]}! Код: {resp.status} Ответ: {data}')
+                        await asyncio.sleep(random.uniform(10, 15))
+                        continue
         logger.success(f'{self.acc.info} Успешно заклеймил {points} поинтов 🎉')
 
     @retry
     async def withdraw(self) -> None:
+        okx_balance = await OKX('').get_okx_ccy_balance('ZETA')
         balance = (await self.acc.get_balance("ZETA"))["balance"]
         amount = balance - random.uniform(*AMOUNT_TO_SAVE)
         if not self.acc.withdraw_address:
             logger.error(f'{self.acc.info} Не указан адрес для вывода!')
             return
-        await self.acc.transfer_zeta(amount, self.acc.withdraw_address)
+        return await self.acc.transfer_zeta(amount, self.acc.withdraw_address)
 
     @retry
     async def check(self) -> dict:
         base_return = {'№': self.acc.account_id, 'Адрес': self.acc.addr, 'Адрес для вывода': self.acc.withdr_addr,
                        'Прокси': self.acc.prox, 'Баланс': f'{(await self.acc.get_balance("ZETA"))["balance"]:.4f}'}
         async with aiohttp.ClientSession(headers=self.headers) as session:
-            async with session.get(f'https://xp.cl04.zetachain.com/v1/get-user-has-xp-to-refresh?address={self.acc.address}') as resp:
+            async with session.get(f'https://xp.cl04.zetachain.com/v1/get-user-has-xp-to-refresh?address={self.acc.address}', proxy=self.acc.proxy) as resp:
                 if resp.status != 200:
-                    if resp.status == 400: text = 'Пуст'
+                    if resp.status == 400: text = 'НеРегнут'
                     elif resp.status == 429: text = 'Подожди'
                     else: text = resp.status
                     return {**base_return, 'Поинтов': text, 'На клейм': text, **{task: text for task in TASKS_NAMES.values()}}
