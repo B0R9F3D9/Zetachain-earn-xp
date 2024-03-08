abbdc1fc3aac5dcc4dd152870ce1f847f20e5309
Уэйд Уилсон <131712860+B0R9F3D9@users.noreply.github.com>
2024-03-08 20:00:52 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -20,30 +20,34 @@ DO_ACTION_ANYWAY: bool = False
 CUSTOM_WAY: list[str] = [
     'okx_withdraw', # Вывод с ОКХ
     'enroll', # Регистрация
-    'transfer', # Перевод самому себе
-    'izumi_swap-ZETA/BNB.BSC', # Свап ZETA/BNB.BSC
-    'izumi_swap-ZETA/ETH.ETH', # Свап ZETA/ETH.ETH
-    'izumi_swap-ZETA/BTC.BTC', # Свап ZETA/BTC.BTC
-    'izumi_liquidity-ZETA/BNB.BSC', # Добавить ликвидность ZETA/BNB.BSC
-    'izumi_liquidity-ZETA/ETH.ETH', # Добавить ликвидность ZETA/ETH.ETH
-    'izumi_liquidity-ZETA/BTC.BTC', # Добавить ликвидность ZETA/BTC.BTC
+    'transfer', # Перевод самому себе $ZETA
+    'izumi_swap-ZETA/BNB.BSC', # Свап iZUMi ZETA/BNB.BSC
+    'izumi_swap-ZETA/ETH.ETH', # Свап iZUMi ZETA/ETH.ETH
+    'izumi_swap-ZETA/BTC.BTC', # Свап iZUMi ZETA/BTC.BTC
+    'eddy_swap-ZETA/BNB.BSC', # Свап Eddy Finance ZETA/BNB.BSC
+    'eddy_swap-ZETA/ETH.ETH', # Свап Eddy Finance ZETA/ETH.ETH
+    'eddy_swap-ZETA/BTC.BTC', # Свап Eddy Finance ZETA/BTC.BTC
+    'izumi_liquidity-ZETA/BNB.BSC', # Добавить iZUMi ликвидность ZETA/BNB.BSC
+    'izumi_liquidity-ZETA/ETH.ETH', # Добавить iZUMi ликвидность ZETA/ETH.ETH
+    'izumi_liquidity-ZETA/BTC.BTC', # Добавить iZUMi ликвидность ZETA/BTC.BTC
     'mint_stzeta', # Минт $stZETA
     'claim', # Клейм поинтов
     'withdraw', # Вывод на адрес для депозита
 ]
 
 # Настройки маршрутов
-MIN_WALLET_BALANCE: float = 0 # Минимальный баланс кошелька в $ZETA, если баланс меньше то будет выводить с ОКХ сумму AMOUNT_TO_WITHDRAW
-TRANSFER_AMOUNT: tuple[float, float] = (0.000001, 0.0001) # Мин, Макс сумма перевода самому себе в $ZETA
-SWAP_AMOUNT: tuple[float, float] = (0.00001, 0.001) # Мин, Макс сумма свапа в $ZETA
-STZETA_MINT_AMOUNT: tuple[float, float] = (0.00001, 0.001) # Мин, Макс сумма минта $stZETA
+MIN_WALLET_BALANCE: float = 0.001 # Минимальный баланс кошелька в $ZETA, если баланс меньше то будет выводить с ОКХ сумму AMOUNT_TO_WITHDRAW
+TRANSFER_AMOUNT: tuple[float, float] = (0.000001, 0.00001) # Мин, Макс сумма перевода самому себе в $ZETA
+IZUMI_SWAP_AMOUNT: tuple[float, float] = (0.0001, 0.001) # Мин, Макс сумма свапа на iZUMi в $ZETA
+EDDY_SWAP_AMOUNT: tuple[float, float] = (5.1, 5.5) # Мин, Макс сумма свапа на Eddy Finance в $USD (минимум 5$)
+STZETA_MINT_AMOUNT: tuple[float, float] = (0.00001, 0.0001) # Мин, Макс сумма минта $stZETA
 AMOUNT_TO_SAVE: tuple[float, float] = (0.02, 0.03) # Мин, Макс сумма для сохранения на кошельке в $ZETA
 AMOUNT_TO_WITHDRAW: tuple[float, float] = (2, 2.2) # Мин, Макс сумма для вывода с ОКХ (минимум 2 $ZETA)
 
 # Прочее
 RPC: str = 'https://zetachain-evm.blockpi.network/v1/rpc/public'
 EXPLORER: str = 'https://zetachain.blockscout.com/tx/'
-REF_LINK: str = 'https://hub.zetachain.com/xp?code=YWRkcmVzcz0weGZjQUM1NGQ4QTc3NGM3OTAwOTJBNzJmNDFkYkYxOTFDODUzOGRCOUEmZXhwaXJhdGlvbj0xNzEwNTg0MjM0JnI9MHg4Mjg3MzBhMDhkZjIzMjcwODliMDEyMWI1YjU4MzA3NGYyNTRlN2UwZTQxMjMzOTcwZDA3YWEwN2ExOTNkYmMyJnM9MHg1YjMzNDQ4MjBjOGY5NTVhNWZiMWEzODdlN2ZlNjc3ZGNiMzE5Mjk2YjUzZTIxYTZjODc5OTExMzhlN2Q1YThhJnY9Mjg%3D'
+REF_LINK: str = 'https://hub.zetachain.com/xp?code=YWRkcmVzcz0weGZjQUM1NGQ4QTc3NGM3OTAwOTJBNzJmNDFkYkYxOTFDODUzOGRCOUEmZXhwaXJhdGlvbj0xNzEyNDAxMDU0JnI9MHhlN2Y2Nzc4NjhhOGVmZmRhMGU2NWUzMDdiNjc2MmQ4YWE2MGQ5YzQ0YzI4ODM1ZTE0MTNiNjg5Y2YyNWNlMGM1JnM9MHg2NTFlNmM2Nzc4YzI5ZWMxMWRiYzk5NTkwMGExNTMzMGNmZTA3YWZiMWUyMGU0ZWE5MTg3MDFiNzIwMTk5ZjQ3JnY9Mjg%3D'
 
 # OKX API (keep it secret)
 OKX_API_KEY: str = ''
